357ac72a43df2e013252b814f0cdcf6fff83ed1c
PatBradley88 <patrick_bradley1@yahoo.co.uk>
2020-03-14 05:03:35 -0700

    Added custom layouts for blogs and portfolios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
-
-  before_action :set_title
-
-  def set_title
-  	@page_title = "DevPortfolio | My Portfolio Website"
-  end
+  include DefaultPageContent
 end
