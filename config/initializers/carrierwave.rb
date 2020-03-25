3eb2b545c76bf16ab9b3764834f026e1da59acdf
PatBradley88 <patrick_bradley1@yahoo.co.uk>
2020-03-25 14:21:06 -0700

    alteration made to carrierwave.rb ->{} removed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,10 @@ CarrierWave.configure do |config|
 
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
-  config.aws_attributes = -> { {
+  config.aws_attributes = {
     expires: 1.week.from_now.httpdate,
     cache_control: 'max-age=604800'
-  } }
+  }
 
   config.aws_credentials = {
     access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
